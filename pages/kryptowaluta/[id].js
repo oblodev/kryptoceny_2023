da4eaaf9b6276a74da7f2cfa203f0b2374674870
Dawid Kostka <dpkostka@gmail.com>
2022-11-14 14:50:51 +0100

    worked on the data fetch2

--- pages/kryptowaluta/[id].js
+++ pages/kryptowaluta/[id].js
@@ -28,7 +28,7 @@ export async function getStaticProps(context) {
 
 export async function getStaticPaths() {
   const res = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
   );
   const data = await res.json();
 
