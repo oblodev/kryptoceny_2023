45986e2ca6233491c4efb1eb66dc73b0ca96a572
Dawid Kostka <dpkostka@gmail.com>
2022-11-17 19:38:43 +0100

    fetched cryptos to 10

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export async function getStaticProps() {
   const poradnikPosts = (await getPoradnikPosts()) || [];
 
   const res = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
   );
   const data = await res.json();
 
