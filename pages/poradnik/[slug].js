be27cb3caa8d7825555933badea4c8a0f9aa2865
oblodev <dpkostka@gmail.com>
2025-01-19 17:05:31 +0100

    poradnik seo

--- pages/poradnik/[slug].js
+++ pages/poradnik/[slug].js
@@ -12,11 +12,39 @@ function poradnikDetails({ data }) {
   return (
     <div className={styles.container}>
       <Head>
+        {/* Page Title */}
+        <title>{data.title} | Poradnik Krypto - KryptoCeny.pl</title>
+        <meta
+          name="description"
+          content={`Poznaj szczegóły poradnika: ${data.title}. ${data.excerpt}`}
+        />
+        <meta
+          name="keywords"
+          content={`Poradnik Krypto, ${data.title}, Kryptowaluty, Bitcoin, Ethereum, Altcoin`}
+        />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content={data.title} />
+        <meta
+          property="og:description"
+          content={`Poznaj szczegóły poradnika: ${data.title}. ${data.excerpt}`}
+        />
+        <meta property="og:image" content={data.poradnikImage.url} />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={`https://www.kryptoceny.pl/poradnik/${data.slug}`} />
+        
+        {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@kryptokurs" />
         <meta name="twitter:title" content={data.title} />
         <meta name="twitter:description" content={data.excerpt} />
         <meta name="twitter:image" content={data.poradnikImage.url} />
+
+        {/* Canonical Link */}
+        <link
+          rel="canonical"
+          href={`https://www.kryptoceny.pl/poradnik/${data.slug}`}
+        />
       </Head>
       <PoradnikDetail poradnik={data} />
       <PriceTicker />
