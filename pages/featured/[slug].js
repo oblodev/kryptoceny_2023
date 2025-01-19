800db6b39f63fb9bab2d77bb8cbcc879d2d812b3
oblodev <dpkostka@gmail.com>
2025-01-19 15:25:52 +0100

    enchanced seo featured posts

--- pages/featured/[slug].js
+++ pages/featured/[slug].js
@@ -14,13 +14,35 @@ function FeaturedPostDetails({ data }) {
   return (
     <div className={styles.container}>
       <Head>
+        {/* Basic Metadata */}
+        <title>{`${data.title} | KryptoCeny.pl - Aktualności`}</title>
+        <meta
+          name="description"
+          content={`Przeczytaj najnowszy artykuł: "${data.title}". Zobacz szczegóły, analizy i opinie na temat kryptowalut.`}
+        />
+        <meta name="keywords" content={`${data.category}, kryptowaluty, analiza kryptowalut, wiadomości kryptowalut`} />
+
+        {/* Open Graph Metadata */}
+        <meta property="og:title" content={data.title} />
+        <meta property="og:description" content={data.excerpt} />
+        <meta property="og:image" content={data.featuredImage.url} />
+        <meta property="og:url" content={`https://www.kryptoceny.pl/featured/${data.slug}`} />
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="KryptoCeny" />
+
+        {/* Twitter Metadata */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@kryptokurs" />
         <meta name="twitter:title" content={data.title} />
         <meta name="twitter:description" content={data.excerpt} />
         <meta name="twitter:image" content={data.featuredImage.url} />
+
+        {/* Canonical URL */}
+        <link rel="canonical" href={`https://www.kryptoceny.pl/featured/${data.slug}`} />
       </Head>
+
       <FeaturedPostDetail post={data} />
+      <PriceTicker />
     </div>
   );
 }
