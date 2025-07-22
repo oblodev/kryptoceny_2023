7667b6aa246bba05c70645383236ffdb60f2c601
oblodev <dpkostka@gmail.com>
2025-07-22 18:20:19 +0200

    refactor styling cryptos

--- components/Cryptos.jsx
+++ components/Cryptos.jsx
@@ -2,12 +2,11 @@ import styles from "../styles/Cryptos.module.scss";
 import { useState } from "react";
 import Image from "next/image";
 import NumberFormat from "react-number-format";
-import { useRouter } from "next/router"; // 1. Import useRouter
-
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 function Cryptos({ cryptoData }) {
-  const router = useRouter(); // 2. Initialize the router
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const formatNumber = (value) => (
@@ -20,13 +19,10 @@ function Cryptos({ cryptoData }) {
     />
   );
 
-
   const handleButtonClick = () => {
     setIsLoading(true);
-    // 3. Use router.push for fast, client-side navigation
     router.push("/kurskryptowalut");
   };
-
 
   return (
     <div className={styles.container}>
@@ -34,115 +30,58 @@ function Cryptos({ cryptoData }) {
         <div className={styles.cryptos}>
           <table className={styles.kryptoTable} aria-label="Tabela kryptowalut">
             <thead>
-              <tr className={styles.kryptoHead}>
-                <th className={styles.kryptoBorder}>#</th>
-                <th className={`${styles.kryptoLeft} ${styles.kryptoBorder}`}>
-                  <p>Kryptowaluta</p>
-                </th>
-                <th className={styles.kryptoBorder}>Kurs</th>
-                <th className={styles.kryptoBorder}>Kurs 1h</th>
-                <th className={`${styles.kryptoBorder} ${styles.hide}`}>
-                  Kurs 24h
-                </th>
-                <th className={`${styles.kryptoBorder} ${styles.hide}`}>
-                  Kurs 7d
-                </th>
-                <th
-                  className={`${styles.kryptoBorder} ${styles.hide} ${styles.tabHide}`}
-                >
-                  Wolumen 24h
-                </th>
-                <th className={styles.kryptoBorder}>Kapitalizacja</th>
+              <tr>
+                <th>#</th>
+                <th className={styles.kryptoLeft}>Kryptowaluta</th>
+                <th>Kurs</th>
+                <th className={styles.hideOnMobile}>Kurs 1h</th>
+                <th className={styles.hideOnMobile}>Kurs 24h</th>
+                <th className={styles.hideOnTablet}>Kurs 7d</th>
+                <th className={styles.hideOnTablet}>Wolumen 24h</th>
+                <th>Kapitalizacja</th>
               </tr>
             </thead>
             <tbody>
               {Array.isArray(cryptoData) && cryptoData.length > 0 ? (
                 cryptoData.map((crypto) => (
-                   <tr key={crypto.id}> 
-                    <td className={styles.kryptoBorder2}>
-                      {crypto.market_cap_rank}
+                  <tr key={crypto.id}>
+                    <td>{crypto.market_cap_rank}</td>
+                    <td className={styles.kryptoName}>
+                      <Image
+                        src={crypto.image}
+                        alt={`${crypto.name} logo`}
+                        width={24}
+                        height={24}
+                      />
+                      <Link href={`/kryptowaluta/${crypto.id}`}>
+                        {crypto.name}
+                      </Link>
                     </td>
-                    <td
-                      className={`${styles.kryptoName} ${styles.kryptoBorder2}`}
-                    >
-                      <div className={styles.kryptoImage}>
-                        <Image
-                          src={crypto.image}
-                          alt={`${crypto.name} logo`}
-                          width={24}
-                          height={24}
-                        />
-                      </div>
-                      {crypto.id ? (
-                        <Link href={`/kryptowaluta/${crypto.id}`}>
-                          <a>{crypto.name}</a>
-                        </Link>
-                      ) : (
-                        <span>{crypto.name}</span>
-                      )}
-                    </td>
-                    <td className={styles.kryptoBorder2}>
-                      {crypto.current_price >= 1
-                        ? crypto.current_price.toFixed(2)
-                        : crypto.current_price.toFixed(5)}
-                      $
-                    </td>
-                    <td className={styles.kryptoBorder2}>
-                      <span
-                        className={
-                          crypto.price_change_percentage_1h_in_currency > 0
-                            ? "green"
-                            : "red"
-                        }
-                      >
-                        {crypto.price_change_percentage_1h_in_currency?.toFixed(
-                          2
-                        )}
-                        %
+                    <td>${crypto.current_price?.toFixed(2)}</td>
+                    <td className={styles.hideOnMobile}>
+                      <span className={crypto.price_change_percentage_1h_in_currency > 0 ? "green" : "red"}>
+                        {crypto.price_change_percentage_1h_in_currency?.toFixed(2)}%
                       </span>
                     </td>
-                    <td className={`${styles.kryptoBorder2} ${styles.hide}`}>
-                      <span
-                        className={
-                          crypto.price_change_percentage_24h > 0
-                            ? "green"
-                            : "red"
-                        }
-                      >
+                    <td className={styles.hideOnMobile}>
+                      <span className={crypto.price_change_percentage_24h > 0 ? "green" : "red"}>
                         {crypto.price_change_percentage_24h?.toFixed(2)}%
                       </span>
                     </td>
-                    <td
-                      className={`${styles.kryptoPrice} ${styles.kryptoBorder2} ${styles.hide}`}
-                    >
-                      <span
-                        className={
-                          crypto.price_change_percentage_7d_in_currency > 0
-                            ? "green"
-                            : "red"
-                        }
-                      >
-                        {crypto.price_change_percentage_7d_in_currency?.toFixed(
-                          2
-                        )}
-                        %
+                    <td className={styles.hideOnTablet}>
+                      <span className={crypto.price_change_percentage_7d_in_currency > 0 ? "green" : "red"}>
+                        {crypto.price_change_percentage_7d_in_currency?.toFixed(2)}%
                       </span>
                     </td>
-                    <td
-                      className={`${styles.kryptoPrice} ${styles.kryptoBorder2} ${styles.hide} ${styles.tabHide}`}
-                    >
+                    <td className={styles.hideOnTablet}>
                       {formatNumber(crypto.total_volume)}$
                     </td>
-                    <td className={`${styles.kryptoPrice} ${styles.kryptoBorder2}`}>
-                      {formatNumber(crypto.market_cap)}$
-                    </td>
+                    <td>{formatNumber(crypto.market_cap)}$</td>
                   </tr>
                 ))
               ) : (
                 <tr>
-                  <td colSpan="8" className={styles.loading}>
-                    Ładowanie danych...
-                  </td>
+                  <td colSpan="8">Ładowanie danych...</td>
                 </tr>
               )}
             </tbody>
@@ -152,13 +91,9 @@ function Cryptos({ cryptoData }) {
           <button
             onClick={handleButtonClick}
             aria-label="Zobacz więcej kursów kryptowalut"
-            disabled={isLoading} // Disable button during loading
+            disabled={isLoading}
           >
-            {isLoading ? (
-              <div className={styles.spinner}></div>
-            ) : (
-              "Zobacz więcej"
-            )}
+            {isLoading ? <div className={styles.spinner}></div> : "Zobacz więcej"}
           </button>
         </div>
       </div>
