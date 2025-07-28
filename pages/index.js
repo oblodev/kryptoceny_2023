0e82f2e5e246ff528346e3d38b57e3d9befa5cfa
oblodev <dpkostka@gmail.com>
2025-07-28 23:19:17 +0200

    added clarity

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,14 @@ export default function Home({
         <meta
           name="google-site-verification"
           content="WotMEVkt3D6C6zrVbDo1LCqAO6gvsDcPbJH1xfBa-TQ"
-        />
+        />#
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "sm5oayz5x8");
+        </script>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
